77a11ff163432776bbd24f4e9d5ce5eeb56f8e22
Kevin Verschaeve <kevin.verschaeve@live.fr>
2025-10-28 17:27:39 +0100

    remove debug tool in prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+export default defineConfig(({ mode }) => ({
+	plugins: [
+		sveltekit(),
+		...(mode === 'development' ? [devtoolsJson()] : [])
+	],
 	resolve: {
 		conditions: ['svelte', 'browser']
 	}
-});
+}));
